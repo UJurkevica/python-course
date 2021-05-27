2ddace96ab56b83c7c4e27d2a957bb66c1a808e3
UJurkevica <una.jurkevica@gmail.com>
2021-05-27 19:30:17 +0100

    Create exercise3.py file

---
+++ session2/exercise3.py
@@ -0,0 +1,16 @@
+spending_1 = 300
+spending_2 = 250
+spending_3 = 500
+
+earning_1 = 1300
+earning_2 = 200
+
+cost_june = spending_1 + spending_2 + spending_3
+
+earnings_june = earning_1 + earning_2
+
+profit_june = earnings_june - cost_june
+
+
+print(f'The grocery shop made £{profit_june} in June')
+print(f'The grocery shop made £{earning_1 + earning_2 - spending_1 - spending_2 - spending_3} in June')
