920a6c2481afb68f37956cea6887a95a7636008f
UJurkevica <una.jurkevica@gmail.com>
2021-05-27 19:08:10 +0100

    exerc a to d added

--- session2/exercise1.py
+++ session2/exercise1.py
@@ -14,3 +14,24 @@ print(ash + " and " + ash_1)
 name = "Ash"
 output = f'Hello, {name}!'
 print(output)
+
+# 1a exerc
+
+# 8$my_first-name = "Raqim"
+my_first_name = "Raqim"
+print(my_first_name)
+
+# 1b
+
+x = y = z = "Blue"
+print(x)
+print(y)
+print(z)
+
+#1c
+
+print(f'Show values: {x=} | {y=} | {z=}')
+
+#1d
+
+print(f'{x.upper()}')
