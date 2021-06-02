4a423b7e13cfb7af6769329dd5493f5e8afe0ad7
UJurkevica <una.jurkevica@gmail.com>
2021-06-02 13:18:11 +0100

    further WIP files added

---
+++ session3/exercise4b.py
@@ -0,0 +1,10 @@
+counter = 0
+total_sum = 0
+while counter <= 500:
+    if (total_sum % 3 == 0):
+        total_sum = total_sum + counter
+    counter = counter + 1
+    print(f'Sum {total_sum}')
+    print(f'Counter: {counter} ')
+
+print(f'The sum of all numbers divisible by three between 1 and 500 is {total_sum}')
