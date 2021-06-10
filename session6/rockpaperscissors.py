1b3aea0fd540b3ef0835e8374b138809a2b616e2
UJurkevica <una.jurkevica@gmail.com>
2021-06-10 18:59:02 +0100

    nothing

---
+++ session6/rockpaperscissors.py
@@ -0,0 +1,12 @@
+import random
+
+def user_input():
+
+    choices = ["r", "p", "s"]
+    my_action = input("Enter you choice! (r, p, s")
+    pc_actions = random.choice(possible_choice)
+    print(f'ou choose {my_action}, computer choise is {pc_actions}.')
+    print("Hi")
+
+    #if my_action == pc_actions:
+        #print("Tie")
