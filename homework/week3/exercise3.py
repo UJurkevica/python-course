c84174daccf1b4b71a664e0b18965ac1fc0aecd4
UJurkevica <una.jurkevica@gmail.com>
2021-06-16 21:34:16 +0100

    improved WIP week3 homework

--- homework/week3/exercise3.py
+++ homework/week3/exercise3.py
@@ -10,25 +10,24 @@ print(split(a))
 #3b
 
 list_in_lists = [[5,3,2], [1,3,2,1], [3,2,3], [10,1,5], [6,7,2]]
-x = len(list_in_lists)
-#index = 0
 
 print(list(map(sum, list_in_lists)))
-print(max(list(map(sum, list_in_lists))))
+# print(max(list(map(sum, list_in_lists))))
 
-print(sum(list_in_lists[0]))
-print(len(list_in_lists))
 
-#while len(list_in_lists) <= x:
+def item_of_list():
+    for item in list_in_lists:
+        sum_of_list(item)
+    return item
 
-for item in list_in_lists:
-    print(item)
-    #sum(item)
-    #print(item)
-    #print(index)
-    #index = index + 1
 
-for i in range(len(list_in_lists)):
-    print(i)
-    
-print(x)
+def sum_of_list(onelist):
+    total = 0
+    l = onelist
+    for n in l:
+        total = total + n
+    #print(f'The list is {l} and the value is sum= {total}')
+    if total == max(list(map(sum, list_in_lists))):
+        print(f'The list with largest sum is list {l}, and sum= {total}')
+
+item_of_list()
