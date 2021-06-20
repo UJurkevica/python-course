55a094d8e1126f7a718c58b76acca003768de145
UJurkevica <una.jurkevica@gmail.com>
2021-06-20 11:55:31 +0100

    session7 file structure updated

--- session7/exercise0b.py
+++ session7/exercise0b.py
@@ -3,7 +3,7 @@ import csv
 with open('session7/pokemon.csv', 'w') as file:
     csvwriter = csv.writer(file)
     csvwriter.writerow(['name', 'age'])
-    csvwriter.writerows([['sally', '20'], ['tpyin', '32'], ['yussra', '63']])
+    csvwriter.writerows([['sally', '20'], ['tpyin', '32'], ['yussra', '63'], ['hishee', '22']])
 
 with open('session7/pokemon.csv', 'r') as file:
     csvreader = csv.reader(file)
