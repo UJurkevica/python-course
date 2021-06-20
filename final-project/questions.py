f96877606df068229c4dcfe1584f7a17d8126a5d
UJurkevica <una.jurkevica@gmail.com>
2021-06-20 13:46:24 +0100

    WIP final-project preliminary approach

---
+++ final-project/questions.py
@@ -0,0 +1,10 @@
+quiz = {
+    1 : {
+        "question" : "What is the first name of Iron man?",
+        "answer" : "Tony"
+    },
+    2 : {
+        "question" : "Who is called the god of lightning in Avengers?",
+        "answer" : "Thor"
+    }
+}
